95fff460809ab66e3bcc5bf2e4fa0029abda7535
bung87 <crc32@qq.com>
2020-06-03 15:29:25 +0800

    o

--- src/sea.nim
+++ src/sea.nim
@@ -33,8 +33,8 @@ proc saveDoc*(doc:string;):string =
   result = $hash(doc)
   let dir = if DataDir.len > 0: DataDir else: getCurrentDir()
   let file = open(dir / "docs.txt", fmAppend)
-  echo result
-  file.write(result & doc & "\n")
+  echo doc
+  file.write(result & " " & doc & "\n")
   file.close
 
 proc indexDocument*(tree:var CritBitTree[seq[string]]; doc:sink string,saveDoc = false) = 
@@ -117,17 +117,15 @@ when isMainModule:
   let tokens = @["A", "crit", "bit", "tree", "form", "radix", "tree", "patricia", "trie"]
   assert toSeq(tokenlize(words)) == tokens
   assert ngrams2key(@["A", "crit", "bit"]) == "A;crit;bit"
-  const Doc1 = """
-  Let’s jump into Rust by working through a hands-on project together! This chapter introduces you to a few common Rust concepts by showing you how to use them in a real program. You’ll learn about let, match, methods, associated functions, using external crates, and more! The following chapters will explore these ideas in more detail. In this chapter, you’ll practice the fundamentals.
+  const Doc1 = """Let’s jump into Rust by working through a hands-on project together! This chapter introduces you to a few common Rust concepts by showing you how to use them in a real program. You’ll learn about let, match, methods, associated functions, using external crates, and more! The following chapters will explore these ideas in more detail. In this chapter, you’ll practice the fundamentals.
   We’ll implement a classic beginner programming problem: a guessing game. Here’s how it works: the program will generate a random integer between 1 and 100. It will then prompt the player to enter a guess. After a guess is entered, the program will indicate whether the guess is too low or too high. If the guess is correct, the game will print a congratulatory message and exit.
   """
-  const Doc2 = """
-  Nim is a statically typed compiled systems programming language. It combines successful concepts from mature languages like Python, Ada and Modula.
+  const Doc2 = """Nim is a statically typed compiled systems programming language. It combines successful concepts from mature languages like Python, Ada and Modula.
   """
  
   var tree: CritBitTree[seq[string]]
-  tree.indexDocument(escape Doc1,true)
-  tree.indexDocument(escape Doc2,true)
+  tree.indexDocument( escape(Doc1,"",""),true)
+  tree.indexDocument( escape(Doc2,"",""),true)
   let ids = tree.retrieveDocumentIds("Nim")
   echo retrieveDocuments(ids)
   let ids2 = tree.retrieveDocumentIds("real program")
